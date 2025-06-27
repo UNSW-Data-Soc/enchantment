dc8566882dad2f953bbeaf7e779c46238a8fb8bb
Ishita Gupta <ishita.lives@gmail.com>
2025-06-27 16:48:14 +1000

    initial schema done

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,5 @@ type Runtime = import("@astrojs/cloudflare").Runtime<Env>;
 declare namespace App {
 	interface Locals extends Runtime {}
 }
+
+DATABASE_URL="postgresql://myuser:password@localhost:5432/crafting_table"
