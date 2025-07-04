b239a0b207426f599de82b637fae316632b67037
Manojvradan <manojvradanbs@gmail.com>
2025-07-04 13:35:59 +1000

    Initial

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -23,3 +23,20 @@ export const CAREERS_GUIDE =
 "https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
 export const CAREERS_GUIDE_2021 =
   "https://drive.google.com/file/d/1-y2Uv3YahnbM9O8Xp1knhRs2qTSDTBJ5/preview";
+
+
+export const DATA_SCI_BLOG =
+"https://docs.google.com/presentation/d/e/2PACX-1vSompDUNtS_lM1clzxcuK8Ho1fKYXcpNPBzCpqi0pjYQiLs59O80lvx5chsbvKOnD9CKy_bk4tfJckT/pubembed?start=false&loop=false&delayms=3000";
+export const DATA_VISUALIZATION =
+"https://docs.google.com/presentation/d/1w5pBnS_Da5whz2tp2n9UEpCZloMm-g1fVUk0VHqrIxw/edit?usp=drive_link";
+export const ETHICS_DATA_SCIENCE =
+"https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
+export const EXPLAINABILITY =
+"https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
+export const MACHINE_LEARNING =
+"https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
+export const ORLANDO =
+"https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
+export const SENTIMENTAL_ANALYSIS =
+"https://drive.google.com/file/d/1iBsJdAYltRyh9b3eBlt9pkXC1gPPwo1c/preview";
+
