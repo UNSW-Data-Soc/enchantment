cf47103acacfe36b67f14e4cf94609941ea22660
Avani Agarwal <avaniagarwal@Avanis-MacBook-Air.local>
2026-06-28 18:52:05 +1000

    centralise nav bar

---
+++ src/components/NavBar/constants.js
@@ -0,0 +1,11 @@
+export const about = "about";
+export const updates = "updates";
+export const sponsorships = "sponsorships";
+export const resources = "resources";
+export const publication = "publication";
+export const whoWeAre = "who we are";
+export const blog = "blog";
+
+export function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
