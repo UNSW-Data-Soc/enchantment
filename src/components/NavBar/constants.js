6a43c33ef0aead6c79f29a88a75bcfebeca5e50e
Avani Agarwal <avaniagarwal@Avanis-MacBook-Air.local>
2026-07-08 04:36:39 +1000

    all 3 locations have the same links now

--- src/components/NavBar/constants.js
+++ src/components/NavBar/constants.js
@@ -24,36 +24,8 @@ export const navbar = [
     text: "Blog",
     linkto: "blog",
   },
-];
-
-// Publications dropdown (laptop + mobile)
-export const publications = [
   {
     text: "Publication",
-    linkto: "/publication",
+    linkto: "publication",
   },
-  {
-    text: "First Year Guide",
-    linkto: "/publication/FirstYearGuide",
-  },
-  {
-    text: "Careers Guide",
-    linkto: "/publication/CareersGuide",
-  },
-  {
-    text: "Blogs",
-    linkto: "/publication/Blogs",
-  },
-];
-
-// footer only
-export const footerLinks = [
-  { text: "About", linkto: "about" },
-  { text: "Who We Are", linkto: "who-we-are" },
-  { text: "Updates", linkto: "updates" },
-  { text: "Sponsorships", linkto: "sponsorships" },
-  { text: "Resources", linkto: "resources" },
-  { text: "Blog", linkto: "blog" },
-  { text: "Publication", linkto: "publication" },
-  { text: "First Year Guide", linkto: "publication/FirstYearGuide" },
 ];
